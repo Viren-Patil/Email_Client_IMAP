dda5c2566897bc209c6c8e6eb1088a3e377b4d4f
rutvikmoharil <rutvikgm18.comp@coep.ac.in>
2020-10-19 23:22:02 +0530

    second commit, updated client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ serverPort = 143
 clientSocket = create_connection((serverName, serverPort))
 response1 = clientSocket.recv(2048)
 print(response1.decode())
-print("1.CAPABILITY    2.LOGIN    3.NOOP    4.LOGOUT")
+print("1.CAPABILITY    2.LOGIN    3.NOOP    4.LOGOUT	5.CREATE	6.DELETE	7.REMANE	8.SELECT	9.EXAMINE	10.STATUS	11.CHECK	12.CLOSE")
 def capability():
 	command = "a001 CAPABILITY\r\n"
 	return command
@@ -23,6 +23,42 @@ def noop():
 def logout():
 	command = "a004 LOGOUT\r\n"
 	return command
+	
+def create(mailbox):
+	command = "a005 CREATE " + mailbox + '\r\n'
+	return command
+
+def delete(mailbox):
+	command = "a006 DELETE " + mailbox + '\r\n'
+	return command
+
+def rename(mailbox, new_name):
+	command = "a007 RENAME " + mailbox + " " + new_name + '\r\n'
+	return command
+
+def select(mailbox):
+	command = "a008 SELECT " + mailbox + '\r\n'
+	return command
+
+def examine(mailbox):
+	command = "a009 EXAMINE " + mailbox + '\r\n'
+	return command
+
+def status(mailbox, data_item_names):
+	command = "a010 STATUS " + mailbox + " (" + data_item_names + ')\r\n'
+	#STATUS name_of_mailbox UIDNEXT MESSAGES
+	return command
+
+def check():
+	command = "a011 CHECK\r\n"
+	return command
+
+def close():
+	command = "a012 CLOSE\r\n"
+	return command
+
+
+	
 
 while True:
 	choice = int(input())
@@ -33,7 +69,31 @@ while True:
 	elif choice == 3:
 		command = noop()
 	elif choice == 4:
-		command = logout()
+		command = logout()	
+	elif choice == 5:
+		mailbox = input("Name of mailbox: ")
+		command = create(mailbox)
+	elif choice == 6:
+		mailbox = input("Name of mailbox to be deleted: ")
+		command = delete(mailbox)
+	elif choice == 7:
+		mailbox = input("Name of mailbox: ")
+		new_name = input("New name: ")
+		command = rename(mailbox, new_name)
+	elif choice == 8:
+		mailbox = input("Name of mailbox: ")
+		command = select(mailbox)
+	elif choice == 9:
+		mailbox = input("Name of mailbox: ")
+		command = examine(mailbox)
+	elif choice == 10:
+		mailbox = input("Name of mailbox: ")
+		data_item_names = input("Data item names: ")
+		command = status(mailbox, data_item_names)
+	elif choice == 11:
+		command = check()
+	elif choice == 12:
+		command = close()
 
 	clientSocket.sendall(command.encode())
 	serverResponse = clientSocket.recv(2048)
